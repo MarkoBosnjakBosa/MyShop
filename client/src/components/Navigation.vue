852a3af24ec9f19caa4164a15c7ee3a87c90225c
Marko Bošnjak <marko.bosnjak.bosa.93@gmail.com>
2021-09-26 13:41:06 +0200

    Update

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -1,32 +1,32 @@
 <template>
     <div id="navigation" class="container-fluid">
         <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarOptions" aria-controls="navbarOptions" aria-expanded="false">
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarOptions" aria-expanded="false">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <button id="toggleSidebar" type="button" class="btn btn-dark" @click="toggleSidebar()"><i class="fas fa-angle-double-left"></i></button>
+            <button type="button" id="toggleSidebar" class="btn btn-dark" @click="toggleSidebar()"><i class="fas fa-angle-double-left"></i></button>
             <div id="navbarOptions" class="collapse navbar-collapse">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#" @click="openHome()">Home</a>
+                        <a href="#" class="nav-link" @click="openHome()">Home</a>
                     </li>
                     <li v-if="!userData.isLoggedIn && !userData.isAdmin" class="nav-item">
-                        <a class="nav-link" href="#" @click="openContact()">Contact</a>
+                        <a href="#" class="nav-link" @click="openContact()">Contact</a>
                     </li>
                     <li v-if="!userData.isLoggedIn" class="nav-item">
-                        <a class="nav-link" href="#" @click="openRegistration()">Registration</a>
+                        <a href="#" class="nav-link" @click="openRegistration()">Registration</a>
+                    </li>
+                    <li v-if="!userData.isLoggedIn" class="nav-item">
+                        <a href="#" class="nav-link" @click="openLogin()">Login</a>
                     </li>
                     <li v-if="userData.isLoggedIn" class="nav-item dropdown">
-                        <a id="userOptions" href="#" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="far fa-user-circle"></i></a>
+                        <a href="#" id="userOptions" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="far fa-user-circle"></i></a>
                         <ul class="dropdown-menu" :class="userData.isAdmin ? 'adminDropdownMenu' : 'userDropdownMenu'" aria-labelledby="userOptions">
-                            <li><a class="dropdown-item" href="#" @click="openProfile()">Profile</a></li>
-                            <li><a class="dropdown-item" href="#" @click="openSetup()">Authentication</a></li>
-                            <li><a v-if="!userData.isAdmin" class="dropdown-item" href="#" @click="openInvoices()">Invoices</a></li>
-                            <li><a class="dropdown-item" href="#" @click="logout()">Log out</a></li>
+                            <li><a href="#" class="dropdown-item" @click="openProfile()">Profile</a></li>
+                            <li><a href="#" class="dropdown-item" @click="openSetup()">Authentication</a></li>
+                            <li><a v-if="!userData.isAdmin" href="#" class="dropdown-item" @click="openInvoices()">Invoices</a></li>
+                            <li><a href="#" class="dropdown-item" @click="logout()">Log out</a></li>
                         </ul>
-                    </li>
-                    <li v-else class="nav-item">
-                        <a class="nav-link" href="#" @click="openLogin()">Login</a>
                     </li>
                     <li v-if="userData.isLoggedIn && !userData.isAdmin" class="nav-item dropdown">
                         <cart></cart>
@@ -39,8 +39,8 @@
 
 <script>
     import checkLogin from "./CheckLogin.vue";
-    import route from "./Route.vue";
     import helper from "./Helper.vue";
+    import route from "./Route.vue";
     import cart from "./Cart.vue";
 
     export default {
@@ -49,17 +49,20 @@
             cart
         },
         data() {
-			return {
+            return {
                 userData: {
                     isLoggedIn: false,
                     username: "",
                     isAdmin: false
                 }
-			}
-		},
+            }
+        },
         methods: {
             getUserData() {
                 this.userData = checkLogin.methods.getUserData();
+            },
+            toggleSidebar() {
+                helper.methods.toggleSidebar();
             },
             openHome() {
                 route.methods.openHome();
@@ -84,12 +87,9 @@
             },
             logout() {
                 route.methods.logout();
-            },
-            toggleSidebar() {
-                helper.methods.toggleSidebar();
             }
         },
-        mounted() {
+        created() {
             this.getUserData();
         }
     }
