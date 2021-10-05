a1785d43ea9c5ae24e5823c24514d24003a4f0fc
Marko Bošnjak <marko.bosnjak.bosa.93@gmail.com>
2021-10-05 21:29:09 +0200

    Update

--- client/src/views/ShopCategory.vue
+++ client/src/views/ShopCategory.vue
@@ -4,30 +4,32 @@
             <sidebar></sidebar>
             <div id="pageStyle">
                 <navigation></navigation>
-                <h1>Shop By Category: {{category.title}}</h1>
-                <form autocomplete="off" class="row productsForm" @submit.prevent="getProducts()">
-                    <div class="mb-3 col-md-6">
-                        <input type="text" id="search" class="form-control" placeholder="Search..." v-model="search"/>
-                    </div>
-                    <div class="mb-3 col-md-2">
-                        <input type="number" id="limit" min="1" class="form-control" placeholder="Limit" v-model="limit"/>
-                    </div>
-                    <div class="mb-3 col-md-2">
-                        <select id="orderBy" class="form-control" v-model="orderBy">
-                            <option value="" selected>Order by</option>
-                            <option value="titleAsc">Title &#129045;</option>
-                            <option value="titleDesc">Title &#129047;</option>
-                            <option value="priceAsc">Price &#129045;</option>
-                            <option value="priceDesc">Price &#129047;</option>
-                            <option value="ratingAsc">Rating &#129045;</option>
-                            <option value="ratingDesc">Rating &#129047;</option>
-                        </select>
-                    </div>
-                    <div class="mb-3 col-md-1">
-                        <button type="submit" class="btn btn-primary md-1">Search</button>
-                    </div>
-                    <div class="mb-3 col-md-1">
-                        <button type="button" class="btn btn-dark" data-toggle="tooltip" :title="'Total: ' + total">{{total}}</button>
+                <h1 data-toggle="tooltip" :title="category.title">Shop By Category: {{category.title}}</h1>
+                <form autocomplete="off" @submit.prevent="getProducts()" novalidate>
+                    <div class="row">
+                        <div class="mb-3 col-md-6">
+                            <input type="text" id="search" class="form-control" placeholder="Search..." v-model="search"/>
+                        </div>
+                        <div class="mb-3 col-md-2">
+                            <input type="number" id="limit" min="1" class="form-control" v-model="limit"/>
+                        </div>
+                        <div class="mb-3 col-md-2">
+                            <select id="orderBy" class="form-control" v-model="orderBy">
+                                <option value="" selected>Order by</option>
+                                <option value="titleAsc">Title &#129045;</option>
+                                <option value="titleDesc">Title &#129047;</option>
+                                <option value="priceAsc">Price &#129045;</option>
+                                <option value="priceDesc">Price &#129047;</option>
+                                <option value="quantityAsc">Quantity &#129045;</option>
+                                <option value="quantityDesc">Quantity &#129047;</option>
+                                <option value="ratingAsc">Rating &#129045;</option>
+                                <option value="ratingDesc">Rating &#129047;</option>
+                            </select>
+                        </div>
+                        <div class="btn-group mb-3 col-md-2">
+                            <button type="submit" class="btn btn-primary md-1">Search</button>
+                            <button type="button" class="btn btn-dark" data-toggle="tooltip" :title="'Total: ' + total">{{total}}</button>
+                        </div>
                     </div>
                 </form>
                 <div class="mb-3 row products">
@@ -36,13 +38,19 @@
                             <img :src="renderImage(product.primaryImage)" :alt="product.primaryImage.name" class="card-img-top" @click="openModal($event)">
                             <div class="card-body">
                                 <h5 class="card-title" data-toggle="tooltip" :title="product.title">{{product.title}}</h5>
-                                <p class="card-text">Price: {{formatNumber(product.price)}}</p>
+                                <p class="card-text">Price: {{formatNumber(product.price)}}<br>
+                                    <i class="fas fa-star" :class="{'checked' : getRating(1, product.rating.averageRating)}"></i>
+                                    <i class="fas fa-star" :class="{'checked' : getRating(2, product.rating.averageRating)}"></i>
+                                    <i class="fas fa-star" :class="{'checked' : getRating(3, product.rating.averageRating)}"></i>
+                                    <i class="fas fa-star" :class="{'checked' : getRating(4, product.rating.averageRating)}"></i>
+                                    <i class="fas fa-star" :class="{'checked' : getRating(5, product.rating.averageRating)}"></i>
+                                </p>
                                 <button type="button" class="btn btn-primary" @click="openViewProduct(product._id)">More...</button>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="pages">
+                <div class="mb-3 pages">
                     <button v-if="page - 1 > 0" type="button" class="btn btn-dark page" @click="loadPage(page - 1)"><i class="fas fa-angle-double-left"></i></button>
                     <button type="button" class="btn btn-dark page">{{page}}</button>
                     <button v-if="page < pagesNumber" type="button" class="btn btn-dark page" @click="loadPage(page + 1)"><i class="fas fa-angle-double-right"></i></button>
@@ -91,12 +99,20 @@
                 }).catch(error => console.log(error));
             },
             getProducts() {
+                if(!Number.isInteger(this.limit) || this.limit < 1) this.limit = 1;
                 var body = {search: this.search, category: this.category._id, page: this.page, limit: this.limit, orderBy: this.orderBy};
                 axios.post(process.env.VUE_APP_BASE_URL + process.env.VUE_APP_SERVER_PORT + "/getProducts", body).then(response => {
                     this.products = response.data.products;
                     this.total = response.data.total;
                     this.pagesNumber = response.data.pagesNumber;
                 }).catch(error => console.log(error));
+            },
+            getRating(rating, averageRating) {
+                if(rating <= averageRating) {
+                    return true;
+                } else {
+                    return false;
+                }
             },
             loadPage(page) {
                 if(page > 0 && page <= this.pagesNumber) {
@@ -132,8 +148,9 @@
         margin-bottom: 20px;
         overflow: hidden;
         white-space: nowrap;
+        cursor: pointer;
     }
-    .productsForm, .products {
+    form, .products {
         margin: auto;
         max-width: 1000px;
     }
@@ -149,8 +166,10 @@
         height: 200px;
         cursor: pointer;
     }
+    .checked {
+        color: #ffa500;
+    }
     .pages {
-        margin: auto;
         text-align: center;
     }
     .page {
