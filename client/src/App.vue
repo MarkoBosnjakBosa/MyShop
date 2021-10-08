f08bceac4e1a8c1a5349c195fefc9766b18f25d0
Marko Bošnjak <marko.bosnjak.bosa.93@gmail.com>
2021-10-08 15:42:41 +0200

    Update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.0/css/all.css">
-		<router-view :key="($route.EditProduct + $route.params.productId) || ($route.ViewProduct + $route.params.productId) || ($route.ShopCategory + $route.params.categoryId) || ''"/>
+		<router-view :key="($route.EditProduct + $route.params.productId) || ($route.ViewProduct + $route.params.productId) || ($route.ShopCategory + $route.params.categoryId) || ($route.Order + $route.params.orderId) || ($route.ViewOrder + $route.params.orderId) || ($route.ViewProfile + $route.params.userId) || ''"/>
 	</div>
 </template>
