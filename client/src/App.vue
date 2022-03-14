861572ca024dc69b2066dd23df37c0d802a10677
Marko Bošnjak <marko.bosnjak.bosa.93@gmail.com>
2022-03-14 21:04:09 +0100

    Update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,3 @@
 <template>
-	<router-view :key="($route.EditProduct + $route.params.productId) || ($route.ViewProduct + $route.params.productId) || ($route.ShopCategory + $route.params.categoryId) || ($route.Order + $route.params.orderId) || ($route.ViewOrder + $route.params.orderId) || ($route.ViewProfile + $route.params.userId) || ''"/>
+	<router-view :key="($route.EditProduct + $route.params.productId) || ($route.ViewProduct + $route.params.productId) || ($route.ShopCategory + $route.params.categoryId) || ($route.Order + $route.params.orderId) || ($route.ViewOrder + $route.params.orderId) || ($route.ViewProfile + $route.params.userId) || ''"></router-view>
 </template>
