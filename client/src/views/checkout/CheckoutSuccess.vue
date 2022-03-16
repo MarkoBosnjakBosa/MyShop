88b4fd2ceb3d29fe5c41703666b2a5d927721142
Marko Bošnjak <marko.bosnjak.bosa.93@gmail.com>
2022-03-16 21:17:20 +0100

    Update

--- client/src/views/checkout/CheckoutSuccess.vue
+++ client/src/views/checkout/CheckoutSuccess.vue
@@ -101,7 +101,7 @@
                 document.getElementById("download").classList.remove("fa-file-download");
                 document.getElementById("download").classList.add("fa-spinner", "fa-spin");
                 axios.get(process.env.VUE_APP_BASE_URL + process.env.VUE_APP_SERVER_PORT + "/downloadInvoice/" + this.orderId).then(response => {
-                     if(response.data.downloaded) {
+                    if(response.data.downloaded) {
                         document.getElementById("download").classList.remove("fa-spinner", "fa-spin");
                         document.getElementById("download").classList.add("fa-file-download");
                         route.methods.downloadFile(response.data.fileName);
